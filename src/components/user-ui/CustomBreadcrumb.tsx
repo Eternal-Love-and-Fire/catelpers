d1546249cac8c723bd1c29695049bfcddcb85b64
Eternal-Love-and-Fire <yevgenkotyk@gmail.com>
2024-05-08 18:21:48 +0300

    creating custom breadcrumb

--- src/components/user-ui/CustomBreadcrumb.tsx
+++ src/components/user-ui/CustomBreadcrumb.tsx
@@ -13,7 +13,7 @@ const CustomBreadcrumb = () => {
     { name: string; path: string }[]
   >([]);
   const location = useLocation();
-
+  console.log(location);
   useEffect(() => {
     // Parse the location and generate breadcrumbs based on the pathname
     const paths = location.pathname.split("/").filter((path) => path);
@@ -27,11 +27,23 @@ const CustomBreadcrumb = () => {
   return (
     <Breadcrumb className="ml-16">
       <BreadcrumbList>
+        <React.Fragment>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link className="capitalize" to={`/`}>
+                Home
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          {location.pathname === "/" ? "" : <BreadcrumbSeparator />}
+        </React.Fragment>
         {breadcrumbs.map((breadcrumb, index) => (
           <React.Fragment key={breadcrumb.path}>
             <BreadcrumbItem>
               <BreadcrumbLink asChild>
-                <Link className="capitalize" to={`${breadcrumb.path}`}>{breadcrumb.name}</Link>
+                <Link className="capitalize" to={`${breadcrumb.path}`}>
+                  {breadcrumb.name}
+                </Link>
               </BreadcrumbLink>
             </BreadcrumbItem>
             {index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
